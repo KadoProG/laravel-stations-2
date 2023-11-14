f1ecff5d608578b094bbe2a0b2c1b81e8c401103
KadoProG <splatrain@gmail.com>
2023-11-15 08:10:01 +0900

    add: 01を終了

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// GET /practiceでpracticeという文字列を返す
+Route::get('practice', function() {
+    return response('practice');
+});
+
+// GET /practice2でpractice2という文字列を、変数を用いて返すこと
+Route::get('practice2', function() {
+    $test = 'practice2';
+    return response($test);
+});
+
+// /practice3とアクセスすると、testという文字列が返ってくるようにページを作成
+Route::get('practice3', function() {
+    $test = 'test';
+    return response($test);
+});
