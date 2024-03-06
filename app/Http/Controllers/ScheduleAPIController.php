4e26f985577b2d3c4b17d702b909830a6f69db37
KadoProG <splatrain@gmail.com>
2024-03-06 09:51:25 +0900

    削除処理の追加

--- app/Http/Controllers/ScheduleAPIController.php
+++ app/Http/Controllers/ScheduleAPIController.php
@@ -25,7 +25,9 @@ class ScheduleAPIController extends Controller
         return redirect('/admin/movies')->with('error', '指定されたIDの映画が見つかりませんでした。');
       }
 
+      $request['movie_id'] = $id;
       $validatedData = $request->validate([
+        'movie_id' => 'required|exists:movies,id',
         'start_time_date' => "required|date",
         'start_time_time' => "required|date_format:H:i",
         'end_time_date' => "required|date",
@@ -35,7 +37,6 @@ class ScheduleAPIController extends Controller
       // ミューテータを使用してstart_timeとend_timeを結合
       $validatedData['start_time'] = $validatedData['start_time_date'] . ' ' . $validatedData['start_time_time'];
       $validatedData['end_time'] = $validatedData['end_time_date'] . ' ' . $validatedData['end_time_time'];
-      $validatedData['movie_id'] = $id;
 
       DB::beginTransaction();
 
@@ -77,7 +78,9 @@ class ScheduleAPIController extends Controller
   public function patch_schedules_update(Request $request, string $id)
   {
     try {
+      $request['id'] = $id;
       $validatedData = $request->validate([
+        'id' => 'required|exists:schedules,id',
         'start_time_date' => "required|date",
         'start_time_time' => "required|date_format:H:i",
         'end_time_date' => "required|date",
@@ -129,27 +132,27 @@ class ScheduleAPIController extends Controller
     }
   }
 
-  // /**
-  //  * 映画の削除
-  //  */
-  // public function movies_delete(Request $request, $id)
-  // {
-  //   // 既存のデータを取得
-  //   $movie = Movie::find($id);
+  /**
+   * スケジュールの削除
+   */
+  public function delete_schedules_delete(Request $request, $id)
+  {
+    // 既存のデータを取得
+    $schedule = Schedule::find($id);
 
-  //   if (!$movie) {
-  //     // 該当のIDのデータが存在しない場合の処理
-  //     abort(404, '指定されたIDの映画が見つかりませんでした。');
-  //   }
+    if (!$schedule) {
+      // 該当のIDのデータが存在しない場合の処理
+      abort(404, '指定されたIDの映画が見つかりませんでした。');
+    }
 
-  //   try {
-  //     // データを削除
-  //     $movie->delete();
+    try {
+      // データを削除
+      $schedule->delete();
 
-  //     return redirect('/admin/movies')->with('success', '映画が削除されました。');
-  //   } catch (\Exception $e) {
-  //     // 例外
-  //     return redirect("/admin/movies")->with('error', 'エラー: ' . $e->getMessage());
-  //   }
-  // }
+      return redirect('/admin/movies')->with('success', 'スケジュールが削除されました。');
+    } catch (\Exception $e) {
+      // 例外
+      return redirect("/admin/movies")->with('error', 'エラー: ' . $e->getMessage());
+    }
+  }
 }
