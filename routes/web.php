6a3ddc7cea3a83237c788a3780601c4d2c496029
KadoProG <splatrain@gmail.com>
2026-03-22 22:12:26 +0900

    feat: add api get practice

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/getPractice', [PracticeController::class, 'getPractice']);
 Route::get('/practice', [PracticeController::class, 'sample']);
 Route::get('/practice2', [PracticeController::class, 'sample2']);
 Route::get('/practice3', [PracticeController::class, 'sample3']);
